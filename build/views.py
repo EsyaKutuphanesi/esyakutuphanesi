f084a9219a3b024624d559d85217c976902e3d03
Yigit Guneli <pkd@hipo.biz>
2013-08-06 02:37:00 +0300

    showing object list on homepage

--- build/views.py
+++ build/views.py
@@ -3,11 +3,15 @@ from flask.ext.login import current_user
 
 from ek import app
 
+from models import User, Role, Category, Thing, Object
+
 @app.route('/')
 def home():
+    objects = Object.query.all()
+
     if current_user.is_anonymous():
-        return render_template('index.html', user=None)
+        return render_template('index.html', user=None, objects=objects)
     else:
-        return render_template('index.html', user=current_user)
+        return render_template('index.html', user=current_user, objects= objects)
 
 
