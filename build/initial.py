1f105b43f4d0ee677ccbdf8c2ae66b2abb5ddf5d
Yigit Guneli <pkd@hipo.biz>
2013-08-06 01:33:56 +0300

    adding everyone to initial users

--- build/initial.py
+++ build/initial.py
@@ -1,6 +1,16 @@
 from ek import db
 from models import users
 
+users_list = [
+    {'email': 'yigiit@gmail.com', 'password': 'ekek', 'name': 'yigit'},
+    {'email': 'umutcanonal@gmail.com', 'password': 'ekek', 'name': 'umut'},
+    {'email': 'aysu@esyakutuphanesi.com', 'password': 'ekek', 'name': 'aysu'},
+    {'email': 'ayse@esyakutuphanesi.com', 'password': 'ekek', 'name': 'ayse'},
+    ]
+
 db.create_all()
-users.create_user(email='yigiit@gmail.com', password='ekek', name='yigit')
+
+for user in users_list:
+    users.create_user(email=user.get('email'), password=user.get('password'), name=user.get('name'))
+
 db.session.commit()
