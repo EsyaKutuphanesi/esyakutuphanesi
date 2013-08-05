831118e498f435287e23aaf094c6547c2b04086f
Yigit Guneli <pkd@hipo.biz>
2013-08-06 01:26:02 +0300

    reverting friend changes

--- build/admin.py
+++ build/admin.py
@@ -2,7 +2,7 @@ from flask.ext.login import current_user
 from flask.ext.superadmin import Admin, model, AdminIndexView
 
 from ek import app, db
-from models import User, Role, Category, Thing, Object, Friends
+from models import User, Role, Category, Thing, Object
 
 class AdminIndexView(AdminIndexView):
     def is_accessible(self):
@@ -16,4 +16,3 @@ admin.register(Role, session=db.session)
 admin.register(Category, session=db.session)
 admin.register(Thing, session=db.session)
 admin.register(Object, session=db.session)
-admin.register(Friends, session=db.session)
