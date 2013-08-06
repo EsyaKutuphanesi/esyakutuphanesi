5eabf8cd16417451e425ecad9ae6a37b4d513189
Yigit Guneli <pkd@hipo.biz>
2013-08-06 16:36:42 +0300

    displaying objects on admin index page

--- build/admin.py
+++ build/admin.py
@@ -1,26 +1,21 @@
 from flask.ext.login import current_user
-from flask.ext.superadmin import Admin, AdminIndexView, BaseView, expose
+from flask.ext.superadmin import Admin, AdminIndexView, expose
 
 from ek import app, db
 from models import User, Role, Category, Thing, Object
 
 
-class AdminView(BaseView):
-    @expose('/')
-    def index(self):
-        objects = [1,2]
-        a = 'd'
-        return self.render('admin/index.html', objects=objects, a=a)
-
-
 class AdminIndexView(AdminIndexView):
+
     def is_accessible(self):
         return current_user.is_authenticated()
 
+    @expose('/')
+    def index(self):
+        objects = Object.query.all()
+        return self.render('admin/index.html', objects=objects)
+
 admin = Admin(app, app.config['NAME'], index_view=AdminIndexView())
-
-admin.add_view(AdminView(name='Index'))
-
 
 admin.register(User, session=db.session)
 admin.register(Role, session=db.session)
