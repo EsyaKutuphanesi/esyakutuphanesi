46da654ceef3678473183c0e4fcba9ba037e14b4
Yigit Guneli <pkd@hipo.biz>
2013-08-08 04:06:43 +0300

    fixing admin_url method for response and request models

--- build/views.py
+++ build/views.py
@@ -3,15 +3,26 @@ from flask.ext.login import current_user
 
 from ek import app
 
-from models import User, Role, Category, Thing, Object
+from models import User, Role, Category, Thing, Object, Response, Request
 
 @app.route('/')
 def home():
     objects = Object.query.all()
+    users = User.query.all()
+    responses = Response.query.all()
+    requests = Request.query.all()
 
     if current_user.is_anonymous():
-        return render_template('index.html', user=None, objects=objects)
+        user = None
     else:
-        return render_template('index.html', user=current_user, objects= objects)
+        user = current_user
+
+    return render_template('index.html',
+                           user=user,
+                           objects=objects,
+                           users=users,
+                           responses=responses,
+                           requests=requests,
+                           )
 
 
