63d426146a2858e707e5da756ed57611a2b12fb7
Didem <didem.yeni@gmail.com>
2014-06-21 19:45:14 +0300

    fb login

--- build/oauth_handler.py
+++ build/oauth_handler.py
@@ -3,6 +3,9 @@ from flask_oauth import OAuth
 from flask.ext.security import login_user
 from models import *
 
+import urllib
+import uuid
+
 FACEBOOK_APP_ID = '1478430419047146'
 FACEBOOK_APP_SECRET = 'a4e998278e99029f86abaa2eb4ef95df'
 
@@ -45,7 +48,12 @@ def facebook_authorized(resp):
 
     session['facebook_token'] = (resp['access_token'], '')
     data = facebook.get('/me').data
-    profile_picture = facebook.get('/me/picture').data
+    profile_picture = 'https://graph.facebook.com/'+data['id']+'/picture?width=1000'
+    # profile_picture = facebook.get('/me/picture').data
+
+    generated_name = str(uuid.uuid1())+'.jpg'
+
+    urllib.urlretrieve(profile_picture, 'static/photos/'+generated_name )
 
     if 'id' in data and 'name' in data \
         and 'email' in data and 'link' in data:
@@ -55,13 +63,13 @@ def facebook_authorized(resp):
         username = data['username']
         user_email = data['email']
         profile_url = data['link']
-        profile_picture = profile_picture['url']
+        # profile_picture = profile_picture['url']
 
         user = User.query.filter(User.email == user_email).first()
         if not user:
             user = users.create_user(name=name,
                                      email=user_email,
-                                     photo=profile_picture,
+                                     photo=generated_name,
                                      password=None,
                                      active=True)
             users.commit()
@@ -74,7 +82,7 @@ def facebook_authorized(resp):
                                     provider_user_id=provider_user_id,
                                     access_token=resp['access_token'],
                                     profile_url=profile_url,
-                                    image_url=profile_picture,
+                                    image_url=generated_name,
                                     full_name=name,
                                     display_name=username
             )
@@ -90,3 +98,7 @@ def facebook_authorized(resp):
             return redirect(next_url)
 
     return redirect("/login")
+
+def get_file_extension(filename):
+    if '.' in filename:
+        return filename.rsplit('.', 1)[1]
