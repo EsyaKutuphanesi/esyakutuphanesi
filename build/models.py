1a7faf84de2fa7faeb66fda1c3ec87a517678ca9
Didem <didem.yeni@gmail.com>
2014-08-18 15:04:40 +0300

    changed some words

--- build/models.py
+++ build/models.py
@@ -315,9 +315,10 @@ users = SQLAlchemyUserDatastore(db, User, Role)
 
 class ExtendedRegisterForm(RegisterForm):
     name = TextField(u'İsim Soyisim', [Required(u'İsmini girmen gerekli')])
-    why = TextAreaField(u'Kendinden ve/veya Eşya Kütüphanesi ne neden geldiğinden kısaca bahsedebilir misin? '
-                        u'Gerçekten tüm cevapları tek tek okuyoruz :) Hobilerin, '
-                        u'buradan beklentilerin, bir ejderhan olsa adını ne koyardın anlat bize!', [
+    why = TextAreaField(u'Kendinden ve/veya Eşya Kütüphanesi\'ne neden geldiğinden kısaca bahsedebilir misin? '
+                        u'</br>Mesela hobilerin, buradan beklentilerin neler? '
+                        u'Bir ejderhan olsa adını ne koyardın? En sevdiğin yemek ne?'
+                        u'</br>Dipnot: Gerçekten tüm cevapları tek tek okuyoruz :)', [
         Length(min=0, max=1000),
         Required(u'Seni daha yakından tanımayı istiyoruz.')
     ])
