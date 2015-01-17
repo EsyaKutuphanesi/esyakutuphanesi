cc7565261c468ffb107751e8884ec80200f6cdd6
Umutcan Önal <umutcanonal@gmail.com>
2015-01-17 11:59:05 +0200

    popup js fix

--- src/esyakutuphanesi/static/js/popup.js
+++ src/esyakutuphanesi/static/js/popup.js
@@ -84,3 +84,31 @@ $(document).ready(function(){
 	});
 
 });
+
+function getCookie(cname) {
+	var name = cname + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0; i<ca.length; i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1);
+		if (c.indexOf(name) == 0) return decodeURIComponent(c.substring(name.length,c.length));
+	}
+	return "";
+}
+
+function setCookie(cname, cvalue, exdays) {
+	var d = new Date();
+	d.setTime(d.getTime() + (exdays*24*60*60*1000));
+	var expires = "expires="+d.toUTCString();
+	document.cookie = cname + "=" + encodeURIComponent(cvalue) + "; " + expires;
+}
+
+function checkCookie() {
+	var popupIsShown=getCookie("popupisshown");
+	if (popupIsShown != "true")
+	{
+		centerPopup();
+		loadPopup();
+		setCookie("popupisshown","true",1);
+	}
+}
